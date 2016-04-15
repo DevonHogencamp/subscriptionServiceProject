cb9ab270b6cf7c28a4d4b0272011b772f573be7f
Devon Hogencamp <devon.hogencamp@west-mec.org>
2016-04-15 15:09:25 -0700

    Final

--- Admin.php
+++ Admin.php
@@ -1,6 +1,31 @@
 <?php
+    //Set username and password
+    $username = 'devonh';
+    $password = 'devon';
+
+    if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) || ($_SERVER['PHP_AUTH_USER'] != $username) || ($_SERVER['PHP_AUTH_PW']) != $password) {
+        //If the username/password are incorrect send back authentication errors
+        header('HTTP/1.1 401 Unauthorized');
+        header('WWW-Authenticate: Basic realm="Guitar Wars"');
+        exit('<h2>Guitar Wars</h2>Sorry, you must enter a valid user name and password to access this page');
+    }
+
     session_start();
     require_once ('Connect.php');
+
+    if (@$_POST['Remove']) {
+        foreach ($_POST['Delete'] as $DeleteID) {
+            $Query = $dbh->prepare("DELETE FROM Songs WHERE SongID = :DeleteID;");
+
+            $Query->execute(
+                array(
+                    'DeleteID' => $DeleteID
+                )
+            );
+        }
+
+        echo "Customer(s) were removerd successfully";
+    }
 ?>
 
 <!DOCTYPE html>
@@ -47,24 +72,24 @@
                     <h4>Select all of the songs you would like to delete</h4>
 
                     <?php
-                        $Query = $dbh->prepare("SELECT * FROM Songs ORDER BY Date ASC, Genre ASC");
+                        $Query = $dbh->prepare("SELECT * FROM Songs");
                         $Query->execute();
                         $Songs = $Query->fetchAll();
                     ?>
 
                     <center>
-                        <form method="post" name="Delete">
+                        <form method="post" name="Remove">
                             <?php
                                 foreach ($Songs as $row) {
                                     echo '<input type="checkbox" value"' . $row['SongID'] . '" name="Delete[]" />';
                                     echo $row['Title'];
-                                    echo " " . $row['Artist'];
-                                    echo " " . $row['Genre'];
-                                    echo " " . $row['UserID'];
+                                    echo "  " . $row['Artist'];
+                                    echo "  " . $row['Genre'];
+                                    echo "  " . $row['UserID'];
                                     echo "<br>";
                                 }
                             ?>
-                            <input type="submit" name="Delete" />
+                            <input type="submit" name="Remove" />
                         </form>
                     </center>
 
